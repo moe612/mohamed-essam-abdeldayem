414d20417ff7786deaddc699207371f7d7c41401
moe612 <117944010+moe612@users.noreply.github.com>
2024-12-14 00:53:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let btnMenu= document.getElementById("btnMenu");
+    let NavLinks=document.querySelector(".links");
+    btnMenu.onclick =function(){
+        btnMenu.classList.toggle("fa-times")
+        NavLinks.classList.toggle("active")
+    }
